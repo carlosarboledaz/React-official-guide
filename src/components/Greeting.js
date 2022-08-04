96496cc3e7f457a3c71fff27cd7614b4f8bb7328
Carlos Arboleda <carlos.arboleda@outlook.com>
2022-08-03 22:03:02 -0500

    Access the state directly without using props

--- src/components/Greeting.js
+++ src/components/Greeting.js
@@ -1,8 +1,7 @@
 import UserGreeting from './UserGreeting';
 import GuestGreeting from './GuestGreeting';
 
-function Greeting(props){
-    const isLoggedIn = props.isLoggedIn;
+function Greeting({isLoggedIn}){
     if (isLoggedIn){
         return <UserGreeting/>
     } else {
